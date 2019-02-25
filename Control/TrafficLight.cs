6d9252f4c1b1cca64afbe42ba7f79486e0b492c7
Greg <klugscheisser1911@live.de>
2019-02-26 00:53:44 +0100

    refactoring

---
+++ Control/TrafficLight.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+
+using UnityEngine;
+
+namespace gk1911.TheGame.Unity.Core
+{
+	internal class TrafficLight : MonoBehaviour
+	{
+		private enum Phase { Init, Prep, Begin, }
+
+		private static bool isAcceptingTraffic = true;
+		private static readonly List<object> traffic = new List<object>();
+
+		private readonly ReadOnlyCollection<Phase> phases = new ReadOnlyCollection<Phase>((Phase[])Enum.GetValues(typeof(Phase)));
+		private readonly BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic
+			| BindingFlags.Instance | BindingFlags.Static
+			| BindingFlags.DeclaredOnly;
+
+		public static void Subscribe(object roadUser)
+		{
+			if (isAcceptingTraffic) {
+				traffic.Add(roadUser);
+			} else {
+				throw new InvalidOperationException("The traffic has already been regulated. " +
+					"Please subscribe at an earlier point in time.");
+			}
+		}
+
+		private TrafficLight() { }
+
+		private void Start()
+		{
+			new List<Phase>(phases).ForEach(phase
+			 => traffic.ForEach(roadUser
+				 => InvokeMethod(phase, roadUser)));
+			isAcceptingTraffic = false;
+			traffic.Clear();
+			Destroy(this);
+		}
+
+		private void InvokeMethod(Phase method, object obj)
+			=> obj.GetType().GetMethod(method.ToString(), bindingFlags)?.Invoke(obj, null);
+	}
+}
